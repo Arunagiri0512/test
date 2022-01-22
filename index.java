ec989b72e9a298dcaf5b71cf648e33e398bb37aa
Arunagiri0512 <arunagiri0512@gmail.com>
2022-01-22 15:13:20 +0530

    lastcommit

--- index.java
+++ index.java
@@ -1,6 +1,10 @@
 class index{
+    public void display(){
+            System.out.println("display");
+    }
     public static void main(String[] args) {
         System.out.println("helo world");
         System.out.println("kemya");
+       
     }
 }
